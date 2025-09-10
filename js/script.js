11105c9789c62a0ac85b27fa1386ab6074dd2f64
CAIODKC <dkccaio@gmail.com>
2025-09-09 21:03:43 -0300

    Teste

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('students').addEventListener('click', function() {
+    window.location.href = 'students.html';
+});
+
+document.getElementById('visitors').addEventListener('click', function() {
+    window.location.href = 'visitors.html';
+});
---
+++ js/students.js
@@ -0,0 +1,3 @@
+document.getElementById('menu_inicial').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
